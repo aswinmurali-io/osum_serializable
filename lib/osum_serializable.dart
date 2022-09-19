8c745a1707f6e5b84695dbe606b8f74cbd483a57
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-09-20 00:19:39 +0530

    exports convertors

---
+++ lib/osum_serializable.dart
@@ -0,0 +1,10 @@
+library osum_serializable;
+
+export 'color.dart';
+export 'datetime.dart';
+export 'directory.dart';
+export 'duration.dart';
+export 'file.dart';
+
+/// Json data type.
+typedef Json = Map<String, dynamic>;
