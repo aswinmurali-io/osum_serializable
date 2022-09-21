6cdb493dbed678616ed58d37ab855d7a74cb4194
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-09-21 07:59:30 +0530

    formats code

--- test/offset_test.dart
+++ test/offset_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:osum_serializable/osum_serializable.dart';
